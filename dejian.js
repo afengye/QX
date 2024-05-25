8d84206a47c002d132e8abdc79c7d0ce96cd0a37
afengye <fengye568@outlook.com>
2024-05-25 20:27:12 +0800

    Update and rename yisou.js to dejian.js

--- dejian.js
+++ dejian.js
@@ -1,18 +1,18 @@
 /******************************
-脚本功能：宜搜小说解锁会员
+脚本功能：得见小说解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-05-23
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/api\.ieasou\.com\/api\/bookapp\/balance\.m url script-response-body https://raw.githubusercontent.com/afengye/QX/main/yisou.js
+^https:\/\/dj\.palmestore\.com\/zyuc\/api\/user\/accountInfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/dejian.js
 [mitm] 
-hostname = mobile-ios-wallpaper-api.zhhainiao.com
+hostname = dj.palmestore.com
 *******************************/
 
 var aFengYe = $response.body;
 var obj =  JSON.parse(aFengYe);
-obj.vipInfo.vip= true, obj.vipInfo.vipStatus = 1;
+obj.body.userInfo.unlockVipInfo.vipType = 1;
 aFengYe = JSON.stringify(obj);
 $done(aFengYe);
