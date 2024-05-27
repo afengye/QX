19d3c70eb11d484e7b8e2a417a4ef1c8a9b6f3c1
afengye <fengye568@outlook.com>
2024-05-27 20:44:44 +0800

    Create peiyinxiu.js
    配音秀

---
+++ peiyinxiu.js
@@ -0,0 +1,22 @@
+/******************************
+脚本功能：配音秀-登陆后解锁会员
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-05-27
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+^https:\/\/iosapi\.peiyinxiu\.com\/Api\/user\/GetMyInfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/peiyinxiu.js
+[mitm] 
+hostname = iosapi.peiyinxiu.com
+*******************************/
+
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+obj.data.is_vip = 1;
+obj.data.vipLevel = 1;
+obj.data.vip_end_time = 4070886812;
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
