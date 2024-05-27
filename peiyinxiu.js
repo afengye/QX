b1a5128321038404c1cc012e0773cf893151320a
afengye <fengye568@outlook.com>
2024-05-27 21:19:01 +0800

    Update peiyinxiu.js

--- peiyinxiu.js
+++ peiyinxiu.js
@@ -19,11 +19,12 @@ if($request.url.indexOf("/GetMyInfo") != -1){
     obj.data.vipLevel = 2;
     obj.data.vip_end_time = 4070886812;
     obj.data.vipEventText = "afengye";
-} else if ($request.url.indexOf("/GetVipDetail") != -1) {
+}
+
+if ($request.url.indexOf("/GetVipDetail") != -1) {
     obj.data.is_vip = 1;
     obj.data.vipLevel = 2;
     obj.data.end_time = 4070886812;
 }
-
 aFengYe = JSON.stringify(obj);
 $done(aFengYe);
