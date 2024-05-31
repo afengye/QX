1ea270e0692616f2503ba43514789c724b2c9da3
afengye <fengye568@outlook.com>
2024-05-31 22:52:27 +0800

    Create jisuan.js

---
+++ jisuan.js
@@ -0,0 +1,25 @@
+/******************************
+脚本功能：计算器HD-解锁会员
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-06-02
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+^https:\/\/www\.40sishi\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/jisuan.js
+[mitm] 
+hostname = www.40sishi.com
+*******************************/
+
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+if($request.url.indexOf("/calculator/user/profile") != -1) {
+      "startTime": 1717167110,
+      "state": 2,
+      "forever": true,
+      "expireTime": 32472115200
+}
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
