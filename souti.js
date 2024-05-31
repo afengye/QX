e2c96316029fd6a9caa5f95ecaedea74f62bd02a
afengye <fengye568@outlook.com>
2024-05-31 21:27:46 +0800

    Update and rename koudai.js to souti.js

---
+++ souti.js
@@ -0,0 +1,33 @@
+/******************************
+脚本功能：拍照搜题-解锁会员
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-06-01
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+^https:\/\/appss\.rhinoxlab\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/souti.js
+[mitm] 
+hostname = appss.rhinoxlab.com
+*******************************/
+
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+if($request.url.indexOf("/app/account/getAccountInfo") != -1) {
+    obj.result.type = "VIP";
+    obj.result.freeFlag = "YES";
+    obj.result.nickname = "afengye"; 
+    obj.result.vipExpireDays = 99999999999;
+    obj.result.vipExpireTime = "2999-06-03 19:44:48";
+    obj.result.vipGroupInfos = [
+       {
+        "groupType" : "TYPE_ONE",
+        "vipType" : "VIP",
+        "autoPay" : "YES"
+      }
+    ];
+}
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
