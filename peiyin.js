408684dd0497125ab6848f1a9db047e6f34d4bc4
afengye <fengye568@outlook.com>
2024-06-06 23:07:51 +0800

    Update and rename pdf.js to peiyin.js

--- peiyin.js
+++ peiyin.js
@@ -1,22 +1,22 @@
 /******************************
-脚本功能：PDF转换助手-登陆后解锁会员
+脚本功能：萌配音-登陆后解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-06-07
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^http:\/\/pdfnew-api\.shoujicad\.com:8086\/api/user url script-response-body https://raw.githubusercontent.com/afengye/QX/main/pdf.js
+^https:\/\/lc\.i7play\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/peiyin.js
 [mitm] 
-hostname = pdfnew-api.shoujicad.com:8086
+hostname = lc.i7play.com
 *******************************/
 
 var aFengYe = $response.body;
 var obj =  JSON.parse(aFengYe);
 
-if($request.url.indexOf("/api/user") != -1) {
-    obj.data.vipExpiryDate = 32472115200;
-    obj.data.forever = true;
+if($request.url.indexOf("/1.1/users") != -1) {
+    obj.username = "afengye";
+    obj.isVip = true;
 }
 
 aFengYe = JSON.stringify(obj);
