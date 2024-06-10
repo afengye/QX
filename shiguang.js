88dc1983d0852d722230b750f080fecb16e14ec0
afengye <fengye568@outlook.com>
2024-06-10 17:27:06 +0800

    Update shiguang.js

--- shiguang.js
+++ shiguang.js
@@ -1,5 +1,5 @@
 /******************************
-脚本功能：时光手账-登陆后解锁会员
+脚本功能：时光手帐-登陆后解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-06-11
@@ -16,6 +16,11 @@ var obj =  JSON.parse(aFengYe);
 
 if($request.url.indexOf("/memcenterlk/member/firstpage.do") != -1) {
     obj.data.vip = true;
+}
+
+
+if($request.url.indexOf("/diary/vip/mem") != -1) {
+    obj.data = "DU5rLTXu2oHwls2k+KiXj+pBYSZcM0qVoiL5OByGN0PJlcgR4udbb4CfLNAl54qbKlW1Zx9DBZhUucaNSgd70ui+qfriswv5Nf/Htb8AqYKiZDYgPpK2DFHgKljlZtYqMyRYNPH9TtXihDf5rwk1aseSrlUhpp93bkaeAYCat45buo3lTm24fPNv3vYHaDcYlXczjtpUkXMlUD989w7ml+Ug8lcd2ckq5SHQZLFn/626gNwpdK7diiOFl1yCnZjqZ5F45ZIIZyxKtqcxCSv8xc/zgmMVt7ussW3bqgSKWXo3bgvpXN7BMXgNHwbYaEAOBLFJczmePPNtW3MJqQEJQLV1BZQAmtwHIHQ2Trg6Rs3AQ1CVDBTBwI6tOVlDd0CApR2oDfcfh+k+nUWq+F9q8g==";
 }
 
 aFengYe = JSON.stringify(obj);
