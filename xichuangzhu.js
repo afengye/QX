42eeff1188d2ec92bc7b14437752ff01124fcf89
afengye <fengye568@outlook.com>
2024-06-11 19:55:32 +0800

    Update and rename kela.js to xichuangzhu.js

--- xichuangzhu.js
+++ xichuangzhu.js
@@ -1,24 +1,24 @@
 /******************************
-脚本功能：克拉壁纸-登陆后解锁会员
+脚本功能：西窗烛-登陆后解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-06-12
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/claritywallpaper\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/kela.js
+^https:\/\/lchttpapi\.xczim\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/xichuangzhu.js
 [mitm] 
-hostname = claritywallpaper.com
+hostname = lchttpapi.xczim.com
 *******************************/
 
 var aFengYe = $response.body;
 var obj =  JSON.parse(aFengYe);
 
-if($request.url.indexOf("/clarity/api/userInfo") != -1) {
-    obj.data.expireTime = 32472115200;
-    obj.data.level = 5;
-    obj.data.nickName = "afengye";
+if($request.url.indexOf("/1.1/users/me") != -1) {
+    obj.username = "afengye";
+    obj.isAdmin = obj.membership = obj.premiumMembership = obj.lifetimeMembership = true;
 }
+
 
 aFengYe = JSON.stringify(obj);
 $done(aFengYe);
