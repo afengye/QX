c554d3394992bde2c21168d55c9b8ce76fede58f
afengye <fengye568@outlook.com>
2024-06-12 23:08:42 +0800

    Update haitun.js

--- haitun.js
+++ haitun.js
@@ -17,10 +17,18 @@ var obj =  JSON.parse(aFengYe);
 if($request.url.indexOf("/app/user/userInfo") != -1) {
     obj.data.nickname = "afengye";
 }
+
 if($request.url.indexOf("/app/vip/status") != -1) {
     obj.data.status = 1;
     obj.data.endTime = "2999-01-01";
 }
+
+if($request.url.indexOf("/app/account/members") != -1) {
+    //obj.data.owner.vipLevel;
+    obj.data.owner.vipStatus = 1;
+}
+
+
 
 aFengYe = JSON.stringify(obj);
 $done(aFengYe);
