4d3c686df578617f62c0a7a829d80c06ad12f141
afengye <fengye568@outlook.com>
2024-06-12 22:58:38 +0800

    Update and rename xiaobao.js to haitun.js

--- haitun.js
+++ haitun.js
@@ -1,21 +1,21 @@
 /******************************
-脚本功能：小宝搜题-解锁会员
+脚本功能：海豚记账本-登陆后解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-06-13
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^http:\/\/soti\.ixyzh\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/xiaobao.js
+^https://book.haitunwallet.com/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/haitun.js
 [mitm] 
-hostname = soti.ixyzh.com
+hostname = book.haitunwallet.com
 *******************************/
 
 var aFengYe = $response.body;
 var obj =  JSON.parse(aFengYe);
 
-if($request.url.indexOf("/ios/isvip") != -1) {
-    obj.data.isVip = "1";
+if($request.url.indexOf("/app/user/userInfo") != -1) {
+    obj.data.nickname = "afengye";
 }
 
 aFengYe = JSON.stringify(obj);
