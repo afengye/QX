6c44c76bc41a24253867fb3e8c336409471ada6c
afengye <fengye568@outlook.com>
2024-06-13 22:02:17 +0800

    Create wanneng.js

---
+++ wanneng.js
@@ -0,0 +1,27 @@
+/******************************
+脚本功能：万能变声器-解锁会员
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-06-14
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+^https:\/\/www\.40sishi\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/wanneng.js
+[mitm] 
+hostname = www.40sishi.com
+*******************************/
+
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+if($request.url.indexOf("/voice/user/profile") != -1) {
+    obj.data.vipState =  {
+        "startTime": "1970-01-01",
+        "state": 1,
+        "forever": true,
+        "expireTime": "2999-01-01"
+    }
+}
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
