281b8721ddb4ab51b5adeab11a4e31d56c13e629
afengye <fengye568@outlook.com>
2024-06-15 00:02:45 +0800

    Update and rename luping.js to bofang.js

--- bofang.js
+++ bofang.js
@@ -1,19 +1,19 @@
 /******************************
-脚本功能：录屏-解锁会员
+脚本功能：万能播放器-解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-06-15
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^http:\/\/sc\.szsszykj\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/luping.js
+^https:\/\/www\.40sishi\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/bofang.js
 [mitm] 
-hostname = sc.szsszykj.com
+hostname = www.40sishi.com
 *******************************/
 
 var aFengYe = $response.body;
 
-if($request.url.indexOf("/api/GetIosVip.php") != -1) {
+if($request.url.indexOf("/seaplayer/user/profile") != -1) {
    aFengYe = aFengYe.replace("\"is_vip\":0", "\"is_vip\":1");
 }
 
