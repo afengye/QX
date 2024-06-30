4ad3af2dcc3818a826ef90c56cd024881e2ffe56
afengye <fengye568@outlook.com>
2024-06-30 11:04:40 +0800

    Update and rename quling.js to yqxj.js

--- yqxj.js
+++ yqxj.js
@@ -1,20 +1,20 @@
 /******************************
-脚本功能：趣铃-解锁会员
+脚本功能：元气AI相机-解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-07-01
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/apiquling\.hengqifeng\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/quling.js
+^https:\/\/api-ios-aicamera\.vipguanjia\.net\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/yqxj.js
 [mitm] 
-hostname = apiquling.hengqifeng.com
+hostname = api-ios-aicamera.vipguanjia.net
 *******************************/
 
 var aFengYe = $response.body;
 var obj =  JSON.parse(aFengYe);
 
-if($request.url.indexOf("/user/getUserByToken") != -1) {
+if($request.url.indexOf("/yqaicamera/mobile/user/info") != -1) {
     obj.data.ifMember = "0";
     obj.data.endDate = "2999-01-01 00:00:00";
     obj.data.grade = "3";
