39eaaf3d78919a87126b7ff89007f83cc56688ac
afengye <fengye568@outlook.com>
2024-07-04 19:23:37 +0800

    Update and rename aixiu.js to manhua.js

---
+++ manhua.js
@@ -0,0 +1,32 @@
+/******************************
+脚本功能：漫画相机-解锁会员
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-07-05
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+^https:\/\/appss\.rhinoxlab\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/manhua.js
+[mitm] 
+hostname = appss.rhinoxlab.com
+*******************************/
+
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+if($request.url.indexOf("/app/account/getAccountInfo") != -1) {
+    obj.result.type = "VIP";
+    obj.result.freeFlag = "YES";
+    obj.result.vipExpireDays = 99999999999;
+    obj.result.vipExpireTime = "2999-01-01 00:00:00";
+    obj.result.vipGroupInfos = [
+       {
+        "groupType" : "TYPE_ONE",
+        "vipType" : "VIP",
+        "autoPay" : "YES"
+      }
+    ];
+}
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
