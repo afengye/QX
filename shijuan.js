71dd9746ca6f0eb57dd6ed7346a1264fb7399b28
afengye <fengye568@outlook.com>
2024-07-07 05:41:19 +0800

    Update shijuan.js

--- shijuan.js
+++ shijuan.js
@@ -6,7 +6,7 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/appss\.baomingding\.com\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/shijuan.js
+^https:\/\/appss\.baomingding\.com\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/shijuan.js
 [mitm] 
 hostname = appss.baomingding.com
 *******************************/
@@ -14,19 +14,17 @@ hostname = appss.baomingding.com
 var aFengYe = $response.body;
 var obj =  JSON.parse(aFengYe);
 
-if($request.url.indexOf("/app/account/getAccountInfo") != -1) {
-    obj.result.type = "VIP";
-    obj.result.freeFlag = "YES";
-    obj.result.vipExpireDays = 99999999999;
-    obj.result.vipExpireTime = "2999-01-01 00:00:00";
-    obj.result.vipGroupInfos = [
-       {
-        "groupType" : "TYPE_ONE",
-        "vipType" : "VIP",
-        "autoPay" : "YES"
-      }
-    ];
-}
+obj.result.type = "VIP";
+obj.result.freeFlag = "YES";
+obj.result.vipExpireDays = 99999999999;
+obj.result.vipExpireTime = "2999-01-01 00:00:00";
+obj.result.vipGroupInfos = [
+   {
+    "groupType" : "TYPE_ONE",
+    "vipType" : "VIP",
+    "autoPay" : "YES"
+  }
+];
 
 aFengYe = JSON.stringify(obj);
 $done(aFengYe);
