bfe8b4acaeb8437dd7e0e8f60adb9f6a63c36383
afengye <fengye568@outlook.com>
2024-07-14 15:54:15 +0800

    Update and rename test.js to elephant.js

--- elephant.js
+++ elephant.js
@@ -1,12 +1,12 @@
 /******************************
-脚本功能：计算器HD-解锁会员
+脚本功能：大象冥想-登陆后解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
-更新时间：2024-06-02
+更新时间：2024-07-15
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/nmeditation\.snailsleep\.net\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/test.js
+^https:\/\/nmeditation\.snailsleep\.net\/meditation.+ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/elephant.js
 [mitm] 
 hostname = nmeditation.snailsleep.net
 *******************************/
@@ -25,7 +25,6 @@ if($request.url.indexOf("/meditation-order/api/order/user/vip/info") != -1) {
 if($request.url.indexOf("/meditation-user/user/get/info") != -1) {
      obj.result.vip = vipFlag;
      obj.result.express = expressTime;
-     obj.result.name = "afengye";
 }
 
 aFengYe = JSON.stringify(obj);
