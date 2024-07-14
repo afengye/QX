6cd61261fba1239a9ca220ba47546b02bf2946b5
afengye <fengye568@outlook.com>
2024-07-14 18:28:48 +0800

    Update and rename caidou.js to tyjs.js

--- tyjs.js
+++ tyjs.js
@@ -1,14 +1,14 @@
 /******************************
-脚本功能：彩豆水印相机-解锁会员
+脚本功能：工资计算器-解锁会员
 脚本作者：afengye
 脚本频道：https://t.me/afengye
-更新时间：2024-07-15
+更新时间：2024-07-16
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/appss\.rhinoxlab\.com\/\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/caidou.js
+^https:\/\/appss\.(zhushengzi|rhinoxlab)\.com\/(\/)?app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/tyjs.js
 [mitm] 
-hostname = appss.rhinoxlab.com
+hostname = appss.rhinoxlab.com,appss.zhushengzi.com
 *******************************/
 
 var aFengYe = $response.body;
