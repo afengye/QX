1635eb257b8186976f106e7a816975ea53fbf179
afengye <fengye568@outlook.com>
2024-07-27 10:32:11 +0800

    Update zymh.js

--- zymh.js
+++ zymh.js
@@ -7,8 +7,9 @@
 *******************************
 [rewrite_local]
 ^https:\/\/apigate\.kaimanhua\.com\/zymk-(userpurchased|getuserinfo)-api\/v1\/(userpurchased|getuserinfo)(\.?)+ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/zymh.js
+^https:\/\/(api-access\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/get_ads|open\.e\.kuaishou\.com\/rest\/e\/v3\/open\/univ$|mi\.gdt\.qq\.com\/gdt_mview\.fcg\?) url reject-200
 [mitm] 
-hostname = apigate.kaimanhua.com
+hostname = apigate.kaimanhua.com,api-access.pangolin-sdk-toutiao.com, mi.gdt.qq.com, open.e.kuaishou.com
 *******************************/
 
 var aFengYe = $response.body;
