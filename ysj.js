11d9bd7284dbadf88d80d840adc40089befb4822
afengye <fengye568@outlook.com>
2024-07-30 19:29:42 +0800

    Create ysj.js

---
+++ ysj.js
@@ -0,0 +1,28 @@
+/******************************
+脚本功能：艺术家-说一门外语-登陆后解锁会员功能
+脚本作者：afengye
+脚本频道：https://t.me/afengye
+更新时间：2024-07-31
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+^https:\/\/api\.memrise\.com\/(.?)+\/me\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/ysj.js
+[mitm] 
+hostname = api.memrise.com
+*******************************/
+
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+var vipInfo = {
+   "is_pro": true
+}
+
+for (let key in obj.profile) {
+  if (vipInfo.hasOwnProperty(key)) {
+     obj.profile[key] = vipInfo[key]
+  }
+}
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
