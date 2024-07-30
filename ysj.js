3b43bd08ed1595da4fa85a2e7e81163981d993fd
afengye <fengye568@outlook.com>
2024-07-30 19:38:35 +0800

    Update ysj.js

--- ysj.js
+++ ysj.js
@@ -1,12 +1,12 @@
-/******************************
+/*************************************************************
 脚本功能：艺术家-说一门外语-登陆后解锁会员功能
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-07-31
 使用声明：️仅供学习交流, 🈲️商业用途
-*******************************
+**************************************************************
 [rewrite_local]
-^https:\/\/api\.memrise\.com\/(.?)+\/me\/ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/ysj.js
+^https:\/\/api\.memrise\.com\/(.?)+\/me\/(\.?)+ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/ysj.js
 [mitm] 
 hostname = api.memrise.com
 *******************************/
@@ -18,11 +18,12 @@ var vipInfo = {
    "is_pro": true
 }
 
-for (let key in obj.profile) {
-  if (vipInfo.hasOwnProperty(key)) {
-     obj.profile[key] = vipInfo[key]
-  }
+if(obj.profile) {
+   obj.profile.is_pro = true;
+} else {
+   obj.is_locked = false;
 }
+
 
 aFengYe = JSON.stringify(obj);
 $done(aFengYe);
