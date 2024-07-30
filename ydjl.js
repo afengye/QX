0c0140fe5fa37e89c37967a249e246ec46ac65d2
afengye <fengye568@outlook.com>
2024-07-30 20:06:33 +0800

    Update and rename ysj.js to ydjl.js

--- ydjl.js
+++ ydjl.js
@@ -1,14 +1,14 @@
-/*************************************************************
-脚本功能：艺术家-说一门外语-登陆后解锁会员功能
+/**********************************
+脚本功能：阅读记录-登陆后解锁会员功能
 脚本作者：afengye
 脚本频道：https://t.me/afengye
 更新时间：2024-07-31
 使用声明：️仅供学习交流, 🈲️商业用途
-**************************************************************
+***********************************
 [rewrite_local]
-^https:\/\/api\.memrise\.com\/(.?)+\/(me|learning_sessions)\/(\.?)+ url script-response-body https://raw.githubusercontent.com/afengye/QX/main/ysj.js
+^https:\/\/app\.yidiansz\.com\/api\/v2\/app\/user\/info url script-response-body https://raw.githubusercontent.com/afengye/QX/main/ydjl.js
 [mitm] 
-hostname = api.memrise.com
+hostname = app.yidiansz.com
 *******************************/
 
 var aFengYe = $response.body;
