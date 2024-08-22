a470d997d8336f03145192b7b9609450ea779c9c
afengye <fengye568@outlook.com>
2024-08-22 23:11:35 +0800

    Update yy.js

--- yy.js
+++ yy.js
@@ -11,4 +11,11 @@
 hostname = app.yiyan.art
 *******************************/
 
-let afengye=$response["\u0062\u006f\u0064\u0079"];let obj=JSON["\u0070\u0061\u0072\u0073\u0065"](afengye);let data=obj["\u0075\u0073\u0065\u0072"]?obj["\u0075\u0073\u0065\u0072"]:obj;data["\u0076\u0069\u0070\u0074\u0079\u0070\u0065"]="\u0033";afengye=JSON["\u0073\u0074\u0072\u0069\u006e\u0067\u0069\u0066\u0079"](obj);$done(afengye);
+var aFengYe = $response.body;
+var obj =  JSON.parse(aFengYe);
+
+var data = obj.user ? obj.user : obj;
+data.viptype = "3";
+
+aFengYe = JSON.stringify(obj);
+$done(aFengYe);
