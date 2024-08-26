6fd11799c830de35b368d012753b737833386e2f
afengye <fengye568@outlook.com>
2024-08-26 21:52:52 +0800

    Update ddqsy.js

--- ddqsy.js
+++ ddqsy.js
@@ -10,4 +10,16 @@
 hostname = kkyun.com
 *******************************/
 
-let obj=JSON["\u0070\u0061\u0072\u0073\u0065"]($response["\u0062\u006f\u0064\u0079"]);let data=obj["\u0075\u0073\u0065\u0072"]?obj["\u0075\u0073\u0065\u0072"]:obj;let vipInfo={"\u0069\u0073\u005f\u0076\u0069\u0070":!![],"\u0073\u0074\u0061\u0074\u0075\u0073":0x1,"\u0076\u0069\u0070\u005f\u006e\u0061\u006d\u0065":"\u5df2\u89e3\u9501\u9ad8\u7ea7\u529f\u80fd"};for(let key in data){if(vipInfo["\u0068\u0061\u0073\u004f\u0077\u006e\u0050\u0072\u006f\u0070\u0065\u0072\u0074\u0079"](key)){data[key]=vipInfo[key];}}$done({"\u0062\u006f\u0064\u0079":JSON['stringify'](obj)});
+let obj =  JSON.parse($response.body);
+let data = obj.user ? obj.user : obj;
+let vipInfo = {
+   "is_vip" : true,
+   "status" : 1,
+   "vip_name" : "已解锁高级功能"
+}
+for (let key in data) {
+  if (vipInfo.hasOwnProperty(key)) {
+     data[key] = vipInfo[key]
+  }
+}
+$done({body: JSON.stringify(obj)});
