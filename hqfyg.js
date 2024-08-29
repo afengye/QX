7322a86e04e74523f3c7361c142b694941abdc2a
afengye <fengye568@outlook.com>
2024-08-29 20:26:45 +0800

    Create hqfyg.js

---
+++ hqfyg.js
@@ -0,0 +1,11 @@
+/*******************************
+脚本功能：环球翻译官-解锁周卡会员
+脚本作者：afengye
+注意事项：需登录⚠
+使用声明：️仅供学习交流, 🈲️商业用途
+********************************
+[rewrite_local]
+^https:\/\/hqfyg\.5exg\.cn\/translation\/getuserinfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/hqfyg.js
+[mitm] 
+hostname = hqfyg.5exg.cn
+*******************************/
