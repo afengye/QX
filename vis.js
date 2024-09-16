b1fb887e453dbdfd57f9452ed65c7eea99a23380
afengye <fengye568@outlook.com>
2024-09-16 20:52:25 +0800

    Update and rename vis to vis.js

--- vis.js
+++ vis.js
@@ -1,13 +1,13 @@
 /*******************************
 脚本功能：vis-解锁订阅
 脚本作者：afengye
-注意事项：
+注意事项：开启脚本再进入软件
 频道地址：https://t.me/afengye
 使用声明：️仅供学习交流, 🈲️商业用途
 ********************************
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/vis
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/vis
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/vis.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/vis.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
