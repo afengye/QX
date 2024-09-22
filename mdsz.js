737efa79263040749f9a3128d8d17e8ed8386855
afengye <fengye568@outlook.com>
2024-09-22 20:29:22 +0800

    Update and rename mnsz.js to mdsz.js

--- mdsz.js
+++ mdsz.js
@@ -6,8 +6,8 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 ********************************
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/mnsz.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/mnsz.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/mdsz.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/mdsz.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
