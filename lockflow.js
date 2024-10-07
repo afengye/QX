d13d11130c681ef1f7d71dbef42d7feeca15c738
afengye <fengye568@outlook.com>
2024-10-07 18:57:44 +0800

    Update lockflow.js

--- lockflow.js
+++ lockflow.js
@@ -6,8 +6,8 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 ********************************
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/lockflow.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/lockflow.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/lockflow.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/lockflow.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
