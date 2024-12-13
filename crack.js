559a98f3ddad800ffa3b62b1f81f0307474d3f5d
afengye <fengye568@outlook.com>
2024-12-13 21:31:27 +0800

    Update crack.js

--- crack.js
+++ crack.js
@@ -80,7 +80,9 @@ if(typeof $response == "undefined") {
       {"app_name":"YPlayer","bundle_id":"net.duoziwei.yplayer","product_id":"net.duoziwei.yplayer.premium","entitlements":["Premium"],"version":"8.3"},
       {"app_name":"Overdue","bundle_id":"moshou.Overdue","product_id":"1","entitlements":["Pro"],"version":"5.5.1"},
       {"app_name":"Tally","bundle_id":"com.rainbow.PlusOne","product_id":"tally.remove_ad","entitlements":["pro"],"version":"3.9.0"},
-      {"app_name":"timetrack","bundle_id":"io.timetrack.ios","product_id":"io.timetrack.auto_renew.1","entitlements":["atimelogger-premium-month"],"version":"1.6.127"}
+      {"app_name":"timetrack","bundle_id":"io.timetrack.ios","product_id":"io.timetrack.auto_renew.1","entitlements":["atimelogger-premium-month"],"version":"1.6.127"},
+      {"app_name":"Monefy","bundle_id":"com.monefyapp.monefy","product_id":"com.monefyapp.subscription.yearly.v2","entitlements":["monefy_premium"],"version":"1.5.1"},
+      {"app_name":"HabitMinder","bundle_id":"","product_id":"habitminder.subscription.yearly.v2","entitlements":["habitminder_premium"],"version":"2.9.0"}
    ];  
    for(let data of list){
      if(bundle_id == data.bundle_id || bundle_id == data.app_name){
