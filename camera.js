c2f85b8c2c208fdc01d529c141ff3fe10393d555
afengye <fengye568@outlook.com>
2024-12-26 12:01:05 +0800

    Create camera.js

---
+++ camera.js
@@ -0,0 +1,13 @@
+/*******************************
+脚本功能：ProCamera专业相机-解锁订阅
+脚本作者：afengye
+注意事项：查看频道说明
+频道地址：https://t.me/afengye
+使用声明：️仅供学习交流, 🈲️商业用途
+********************************
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/camera.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/camera.js
+[mitm] 
+hostname = api.revenuecat.com
+*******************************/
