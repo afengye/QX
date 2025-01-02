8b1d244451478d7e147a8ff2262486ecc51fb3cd
afengye <fengye568@outlook.com>
2025-01-02 21:39:12 +0800

    Update crack.js

--- crack.js
+++ crack.js
@@ -104,7 +104,9 @@ if(typeof $response == "undefined") {
       {"app_name":"Thiro","bundle_id":"io.github.ctlvie.atelerix","product_id":"atelerix_pro_lifetime","entitlements":["pro"],"version":"1.3.12"},
       {"app_name":"PROVPN","bundle_id":"com.adpsafe.provpnwebshield","product_id":"com.provpn.year","entitlements":["pro"],"version":"2.0.1"},
       {"app_name":"RunKit","bundle_id":"com.qeezon.RunKit","product_id":"runkit_19.99_1y","entitlements":["Premium"],"version":"1.24"},
-      {"app_name":"Fiery%20Feeds","bundle_id":"net.voidstern.fiery-feeds","product_id":"net.voidstern.fieryfeeds.yearly.trial","entitlements":["com.fieryfeeds.pro"],"version":"2.12.5"}
+      {"app_name":"Fiery%20Feeds","bundle_id":"net.voidstern.fiery-feeds","product_id":"net.voidstern.fieryfeeds.yearly.trial","entitlements":["com.fieryfeeds.pro"],"version":"2.12.5"},
+      {"app_name":"Focused%20iOS","bundle_id":"net.voidstern.focused","product_id":"net.voidstern.focused.yearly.trial","entitlements":["Pro"],"version":"1.9.2"},
+      {"app_name":"Any%20IPTV%20Player","bundle_id":"com.anyiptvplayer.cihan","product_id":"lifetimepremium","entitlements":["premium"],"version":"6.9.7"}
    ];  
    for(let data of list){
      if(bundle_id == data.bundle_id || bundle_id == data.app_name){
