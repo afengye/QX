2ffd856cbf11c482fe225af0254aafbe3f6cbea0
afengye <fengye568@outlook.com>
2025-01-21 17:52:09 +0800

    Update crack.js

--- crack.js
+++ crack.js
@@ -120,7 +120,8 @@ if(typeof $response == "undefined") {
       {"app_name":"Kylin","bundle_id":"lan.kylin","product_id":"pro_life","entitlements":["pro"],"version":"1.27.5"},
       {"app_name":"OneBox","bundle_id":"","product_id":"com.ziheng.pandora.lifetime","entitlements":["all"],"version":"2.2.1"},
       {"app_name":"Echo","bundle_id":"com.LEMO.LemoFM","product_id":"com.LEMO.LemoFm.plus.lifetime.11","entitlements":["Lifetime","PLUS"],"version":"1.11.1"},
-      {"app_name":"Aphrodite","bundle_id":"com.ziheng.aphrodite","product_id":"com.ziheng.aphrodite.lifetime","entitlements":["all"],"version":"2.0.7"}
+      {"app_name":"Aphrodite","bundle_id":"com.ziheng.aphrodite","product_id":"com.ziheng.aphrodite.lifetime","entitlements":["all"],"version":"2.0.7"},
+      {"app_name":"ArchiveList","bundle_id":"com.jy.ArchiveBox","product_id":"com.jy.ArchiveBox.pro_1","entitlements":["pro_life"],"version":"2.6.1"}
    ];  
    for(let data of list){
      if(bundle_id == data.bundle_id || bundle_id == data.app_name){
