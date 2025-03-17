9ab14427f9f32a3be676d1b27c7e850047d83bb2
afengye <fengye568@outlook.com>
2025-03-17 19:01:35 +0800

    Update shijuan.js

--- shijuan.js
+++ shijuan.js
@@ -6,7 +6,7 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 *******************************
 [rewrite_local]
-^https:\/\/appss\.baomingding\.com\/\/app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/shijuan.js
+^https:\/\/appss\.baomingding\.com\/(\/)?app\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/afengye/QX/main/shijuan.js
 [mitm] 
 hostname = appss.baomingding.com
 *******************************/
