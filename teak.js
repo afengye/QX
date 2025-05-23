b57f3b733cf2ce2956d3e054eaecaca91474460d
afengye <fengye568@outlook.com>
2025-05-23 23:35:18 +0800

    Update teak.js

--- teak.js
+++ teak.js
@@ -6,10 +6,10 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 ********************************
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/teak.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/teak.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/teak.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/teak.js
 [mitm] 
-hostname = api.revenuecat.com
+hostname = api.revenuecat.com,api.rc-backup.com
 *******************************/
 
 let obj = {};
